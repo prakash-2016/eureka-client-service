c1aa4bdf3987972b36e580f4c54890d20a950399
prakash-2016 <prakash.chandra.mailbox@gmail.com>
2018-05-11 17:22:18 +0530

    Updated for unknown host

--- stockservice/src/main/java/com/services/stock/StockResource.java
+++ stockservice/src/main/java/com/services/stock/StockResource.java
@@ -1,8 +1,7 @@
-package com.services.controller;
+package com.services.stock;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -10,28 +9,30 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
-import yahoofinance.quotes.stock.StockQuote;
 
-@RestController
+
 //@RequestMapping("/rest/stock")
+@RestController
 public class StockResource {
 
-    @Autowired
+	@Autowired
     RestTemplate restTemplate;
 
     @GetMapping("name/{username}")
     public List<String> getStock(@PathVariable("username") final String userName) {
         System.out.println("In get stock"+userName);
-        ResponseEntity<List<String>> quoteResponse = restTemplate.exchange("http://localhost:8300/rest/db/" + userName, HttpMethod.GET,
+       /* ResponseEntity<List<String>> quoteResponse = restTemplate.exchange("http://localhost:8300/rest/db/" + userName, HttpMethod.GET,
+                null, new ParameterizedTypeReference<List<String>>() {
+                });*/
+        
+        ResponseEntity<List<String>> quoteResponse = restTemplate.exchange("http://db-service/rest/db/" + userName, HttpMethod.GET,
                 null, new ParameterizedTypeReference<List<String>>() {
                 });
-
         
         List<String> quotes = quoteResponse.getBody();
         return quotes;
--- stockservice/src/main/java/com/services/stock/StockserviceApplication.java
+++ stockservice/src/main/java/com/services/stock/StockserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 @EnableEurekaClient
 @SpringBootApplication
-@ComponentScan("com.services.*")
+//@ComponentScan("com.services.*")
 public class StockserviceApplication {
 
 	public static void main(String[] args) {
